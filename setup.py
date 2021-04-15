52876147f370607a733599a74e52482179ff35b9
Ulmschneider <52208504+chris-ulmy@users.noreply.github.com>
2021-04-15 14:05:58 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hrmtools",
+    version="0.0.1",
+    author="Christopher Ulmschneider",
+    author_email="ulmschneider.chris@gmail.com",
+    description="hrmtools for plotting and data handling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["pysimplegui",
+                      "pandas",
+                      "matplotlib",
+                      "numpy"],
+    url="https://github.com/chris-ulmy/hrmtools.git",
+    packages=setuptools.find_namespace_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
